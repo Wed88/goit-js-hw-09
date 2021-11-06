fb95872bb004549c403c856d309a010de906a07d
Dmitriy Dudnik <wed2288@gmail.com>
2021-11-06 18:10:28 +0200

    выполняет 1 ДЗ

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+let isActive = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  isActive = false;
+}
